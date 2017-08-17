ef901cd64b2537149ec40268fe5f28894611416c
Simonas <simonas.sank@gmail.com>
2017-08-17 18:30:23 +0000

    Commented out example

--- example_ws2811.go
+++ example_ws2811.go
@@ -1,19 +1,19 @@
-package ws2811
+// package main
 
-import (
-    "os"
-    "github.com/simonassank/go_ws2811"
-    "strconv"
-)
+// import (
+//     "os"
+//     "github.com/simonassank/go_ws2811"
+//     "strconv"
+// )
 
-func main() {
-    color, _ := strconv.ParseUint(os.Args[1], 0, 64)
+// func main() {
+//     color, _ := strconv.ParseUint(os.Args[1], 0, 64)
 
-    ws2811.Init(18, 144, 255)
+//     ws2811.Init(18, 144, 255)
     
-    for i := 0; i < 144; i++ {
-        ws2811.SetLed(i, uint32(color))
-    }
+//     for i := 0; i < 144; i++ {
+//         ws2811.SetLed(i, uint32(color))
+//     }
 
-    ws2811.Render()
-}
+//     ws2811.Render()
+// }
