0e46fe4f670271255a368ceb76b11ba43375be47
Simonas <simonas.sank@gmail.com>
2017-08-17 18:39:22 +0000

    Updated Example

--- examples/example_ws2811.go
+++ examples/example_ws2811.go
@@ -4,15 +4,17 @@ import (
     "os"
     "github.com/simonassank/go_ws2811"
     "strconv"
+    _ "fmt"
 )
 
 func main() {
     color, _ := strconv.ParseUint(os.Args[1], 0, 64)
-
+    correction, _ := strconv.ParseUint(os.Args[2], 10, 64)
+    isCorrectionEnabled := correction > 0
     ws2811.Init(18, 144, 255)
-    
+
     for i := 0; i < 144; i++ {
-        ws2811.SetLed(i, uint32(color))
+        ws2811.SetLed(i, uint32(color), isCorrectionEnabled)
     }
 
     ws2811.Render()
