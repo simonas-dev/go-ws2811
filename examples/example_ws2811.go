ecd78a9cc86795640c93bae08a34f7318aea6870
Simonas <simonas.sank@gmail.com>
2017-10-15 12:35:37 +0000

    Example

--- examples/example_ws2811.go
+++ examples/example_ws2811.go
@@ -7,13 +7,17 @@ import (
     _ "fmt"
 )
 
+var (
+led_cnt = 144*4
+)
+
 func main() {
     color, _ := strconv.ParseUint(os.Args[1], 0, 64)
     correction, _ := strconv.ParseUint(os.Args[2], 10, 64)
     isCorrectionEnabled := correction > 0
-    ws2811.Init(18, 144, 255)
+    ws2811.Init(18, led_cnt, 255)
 
-    for i := 0; i < 144; i++ {
+    for i := 0; i < led_cnt; i++ {
         ws2811.SetLed(i, uint32(color), isCorrectionEnabled)
     }
 
